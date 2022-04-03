bd3849df0db506ba03db398e24666e3851312a9a
Abhijeet kumar <80936610+Abhijeetbyte@users.noreply.github.com>
2022-04-03 15:17:31 +0530

    Create main_infinite.py
    fastest, program for generating collatz conjecture sequence ( no graph)

---
+++ scripts/#Variables  number = 1 # initial positive number iterations = 0 # to count iteration in while loop   #Interaction print('\n Do you want to start from a specific number [y/n] ?') option_number=str(input(' default is 1, enter y or n to continue : '))  if (option_number == 'Y' or option_number == 'y'):       number =int(input(' enter the number : '))  print('\n Press Ctrl + C any time to quit \n') start=str(input(' Press Enter to begin ! \n'))  num = number  while True:            if(num%2) == 0:  #if number is even make it odd         num = int(num/main_infinite.py
@@ -0,0 +1,40 @@
+
+#Variables
+
+number = 1 # initial positive number
+iterations = 0 # to count iteration in while loop
+
+
+#Interaction
+print('\n Do you want to start from a specific number [y/n] ?')
+option_number=str(input(' default is 1, enter y or n to continue : '))
+
+if (option_number == 'Y' or option_number == 'y'):
+ 
+    number =int(input(' enter the number : '))
+
+print('\n Press Ctrl + C any time to quit \n')
+start=str(input(' Press Enter to begin ! \n'))
+
+num = number
+
+while True:
+     
+     if(num%2) == 0:  #if number is even make it odd
+        num = int(num/2)
+        iterations += 1
+
+     else: 
+         num = int(num*3+1)  #if number is odd make it even
+         iterations += 1
+ 
+     if(num == 1): 
+        print(' Number : ',number, ', Iterations : ',iterations, ', True ', '\n' )
+        number += 1
+        iterations=0
+        num = number
+
+     elif(num == 0): # conjecture break , alert!
+         print(' Number : ', number -1,', False ', '\n' )
+         break #exit loop
+
